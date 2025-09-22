8f2af5a6c6e4bfe6ed5299a70a0f5cd8fedd8768
GustavoMiranda2 <101488574@georgebrown.ca>
2025-09-22 19:52:58 -0400

    Add Lab03 to the Repo

---
+++ Lab03/index.js
@@ -0,0 +1,47 @@
+var http = require("http");
+var employeeModule = require("./Employee");
+
+console.log("Lab 03 - NodeJs");
+
+
+const port = process.env.PORT || 8081;
+
+
+const server = http.createServer((req, res) => {
+    if (req.method !== 'GET') {
+        res.writeHead(405, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({error: http.STATUS_CODES[405]}));
+    } else {
+        if (req.url === '/') {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end("<h1>Welcome to Lab Exercise 03</h1>");
+            return;
+        }
+
+        if (req.url === '/employee') {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(employeeModule.getEmployees()));
+            return;
+        }
+
+        if (req.url === '/employee/names') {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(employeeModule.getEmployeeNames()));
+            return;
+        }
+
+        if (req.url === '/employee/totalsalary') {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({total_salary: employeeModule.getTotalSalary()}));
+            return;
+        }
+
+        
+        res.writeHead(404, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({error: http.STATUS_CODES[404]}));
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
